5c83403f1b19cb8604fe33244d01e379d17cf495
Dennys Azevedo <dennys.bd@gmail.com>
2018-12-26 17:49:32 -0300

    account monthly shoppings

--- incomings/api/v1/serializers.py
+++ incomings/api/v1/serializers.py
@@ -2,7 +2,6 @@ from rest_framework.serializers import ModelSerializer
 from incomings.models import Incoming
 
 class IncomingSerializer(ModelSerializer):
-
     class Meta:
         model = Incoming
         fields = ('id', 'name', 'value', 'account')
